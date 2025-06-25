bfae2e08a6de4b472412117a6c2d2c90bcb1c08f
Ashish <ashish19112016@gmail.com>
2025-06-25 20:04:00 +0200

    style: auto-formating Terraform files

--- main.tf
+++ main.tf
@@ -24,28 +24,28 @@ module "bootstrap" {
 module "aks" {
   source = "./modules/aks" # path to your aks module folder
 
-  resource_group_name        = var.resource_group_name
-  location                   = var.location
-  log_analytics_workspace_id = module.monitoring.log_analytics_workspace_id
-  chatui_namespace           = var.chatui_namespace
-  cluster_name               = var.aks_cluster_name
-  node_count                 = var.node_count
-  node_vm_size               = var.vm_size
-  network_plugin             = var.network_plugin
-  load_balancer_sku          = var.load_balancer_sku
-  oms_agent_enabled          = var.oms_agent_enabled
-  oms_agent_workspace_id     = var.oms_agent_workspace_id
+  resource_group_name             = var.resource_group_name
+  location                        = var.location
+  log_analytics_workspace_id      = module.monitoring.log_analytics_workspace_id
+  chatui_namespace                = var.chatui_namespace
+  cluster_name                    = var.aks_cluster_name
+  node_count                      = var.node_count
+  node_vm_size                    = var.vm_size
+  network_plugin                  = var.network_plugin
+  load_balancer_sku               = var.load_balancer_sku
+  oms_agent_enabled               = var.oms_agent_enabled
+  oms_agent_workspace_id          = var.oms_agent_workspace_id
   api_server_authorized_ip_ranges = var.api_server_authorized_ip_ranges
-  identity_type              = var.identity_type
-  addon_profile              = var.addon_profile
-  network_profile            = var.network_profile
-  kube_config                = var.kube_config
-  kube_config_context        = var.kube_config_context
-  kube_config_path           = var.kube_config_path
-  kube_config_content        = var.kube_config_content
-  kube_config_sensitive      = var.kube_config_sensitive
-  kube_config_output         = var.kube_config_output
-  aks_version                = var.aks_version
+  identity_type                   = var.identity_type
+  addon_profile                   = var.addon_profile
+  network_profile                 = var.network_profile
+  kube_config                     = var.kube_config
+  kube_config_context             = var.kube_config_context
+  kube_config_path                = var.kube_config_path
+  kube_config_content             = var.kube_config_content
+  kube_config_sensitive           = var.kube_config_sensitive
+  kube_config_output              = var.kube_config_output
+  aks_version                     = var.aks_version
 }
 
 module "aks_rbac" {
