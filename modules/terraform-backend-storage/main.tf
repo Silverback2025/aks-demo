5d5c2253f010b6530c31d6e96241ad022e41952e
Ashish Sadhuka <ashish852004@gmail.com>
2025-06-25 19:37:07 +0200

    Update main.tf

--- modules/terraform-backend-storage/main.tf
+++ modules/terraform-backend-storage/main.tf
@@ -7,6 +7,7 @@ resource "azurerm_storage_account" "sa" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
+  min_tls_version = var.min_tls_version
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
@@ -20,4 +21,5 @@ resource "azurerm_storage_container" "tfstate" {
   storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
 }
+
 
