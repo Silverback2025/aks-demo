c0fa5e352bb4768c501b96dfc2856a3e8088a0d1
Ashish Sadhuka <ashish852004@gmail.com>
2025-06-25 17:07:15 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "resource_group_name" {
-  default = "aks-rg"
+  description = "Resource group name"
+  default = "string"
 }
 
 variable "subscription_id" {
@@ -8,11 +9,13 @@ variable "subscription_id" {
 }
 
 variable "location" {
-  default = "westeurope"
+  description = "Azure region"
+  default = "string"
 }
 
-variable "aks_cluster_name" {
-  default = "aks-cluster"
+variable "aks_cluster_name" 
+  description = "Name of the AKS cluster"
+  default = "string"
 } 
 
 variable "log_analytics_workspace_id" {
@@ -23,55 +26,46 @@ variable "log_analytics_workspace_id" {
 variable "chatui_namespace" {
   description = "Namespace for Chat UI"
   type        = string
-  default     = "chatui"
 }
 
 variable "node_count" {
   description = "Number of nodes in the AKS cluster"
   type        = number
-  default     = 3
 }
 
 variable "vm_size" {
   description = "VM size for nodes"
   type        = string
-  default     = "Standard_DS2_v2"
 }
 
 variable "network_plugin" {
   description = "Network plugin to use"
   type        = string
-  default     = "azure"
 }
 
 variable "load_balancer_sku" {
   description = "Load Balancer SKU"
   type        = string
-  default     = "standard"
 }
 
 variable "oms_agent_enabled" {
   description = "Enable OMS Agent"
   type        = bool
-  default     = true
 }
 
 variable "oms_agent_workspace_id" {
   description = "Workspace ID for OMS Agent"
   type        = string
-  default     = ""
 }
 
 variable "identity_type" {
   description = "Identity type for the cluster"
   type        = string
-  default     = "SystemAssigned"
 }
 
 variable "addon_profile" {
   description = "Addon profile for AKS"
   type        = any
-  default     = {}
 }
 
 variable "network_profile" {
@@ -83,79 +77,69 @@ variable "network_profile" {
 variable "kube_config" {
   description = "Kube config"
   type        = any
-  default     = null
 }
 
 variable "kube_config_context" {
   description = "Kube config context"
   type        = string
-  default     = ""
 }
 
 variable "kube_config_path" {
   description = "Kube config path"
   type        = string
-  default     = ""
 }
 
 variable "kube_config_content" {
   description = "Kube config content"
   type        = string
-  default     = ""
 }
 
 variable "kube_config_sensitive" {
   description = "Sensitive kube config flag"
   type        = bool
-  default     = false
 }
 
 variable "kube_config_output" {
   description = "Output kube config flag"
   type        = bool
-  default     = true
 }
 
 variable "aks_version" {
   description = "AKS version"
   type        = string
-  default     = "1.25.0"
 }
 
 variable "admin_group_object_ids" {
   description = "Admin group object IDs for RBAC"
   type        = list(string)
-  default     = []
 }
 
 variable "enable_azure_rbac" {
   description = "Enable Azure RBAC"
   type        = bool
-  default     = false
 }
 
 variable "azure_rbac_enabled" {
   description = "Azure RBAC enabled flag"
   type        = bool
-  default     = false
 }
 
 variable "azure_rbac_mode" {
   description = "Azure RBAC mode"
   type        = string
-  default     = ""
 }
 
 variable "dns_prefix" {
+  description = "DNS prefix"
   type = string
 }
 
-variable "kubernetes_version" {
+variable "kubernetes_version" 
+  description = "Kubernetes version"
   type = string
 }
 
 variable "node_vm_size" {
   description = "Size of the virtual machines in the default node pool"
   type        = string
-  default     = "Standard_DS2_v2"
 }
