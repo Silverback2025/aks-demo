3fc503eb676424c43f90f324b2238655405d7f4e
Ashish Sadhuka <ashish852004@gmail.com>
2025-06-26 06:57:01 +0200

    Update main.tf

--- modules/bootstrap/main.tf
+++ modules/bootstrap/main.tf
@@ -4,17 +4,11 @@ resource "null_resource" "bootstrap" {
   provisioner "local-exec" {
     command = <<EOT
       echo "Setting up kubectl config"
-      az aks get-credentials --resource-group ${var.resource_group_name} --name ${var.aks_cluster_name} --overwrite-existing
-
-      echo "Installing ArgoCD"
-      kubectl create namespace argocd || true
-      kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+      az aks get-credentials --resource-group ${var.resource_group_name} --name ${var.aks_cluster_name} --overwrite-existing --admin
 
       echo "Installing NGINX Ingress"
       kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.9.4/deploy/static/provider/cloud/deploy.yaml
 
-      echo "Applying HuggingFace Chat ArgoCD app"
-      kubectl apply -f ../../argocd-applications/huggingface-chat.yaml
     EOT
   }
 
