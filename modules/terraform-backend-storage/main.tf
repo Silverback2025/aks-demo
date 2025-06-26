f97f3c161d1a24ea21c856bb1ecdac20bb85d89b
Ashish <ashish19112016@gmail.com>
2025-06-26 03:31:10 +0200

    typo errors

--- modules/terraform-backend-storage/main.tf
+++ modules/terraform-backend-storage/main.tf
@@ -1,7 +1,3 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
 
 resource "azurerm_storage_account" "sa" {
   name                     = var.storage_account_name
